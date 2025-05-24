e79a0fd161a73d3de6e2d7a9986bb29a1a6584b0
Kalenga243 <kevinkalenga10@gmail.com>
2025-05-24 21:49:56 +0200

    Navbar structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import {Inter} from 'next/font/google'
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
   title: "HomeAway",
@@ -26,9 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={inter.className}
       >
-        {children}
+        <Navbar />
+        <main className="container py-10">
+           {children}
+        </main>
       </body>
     </html>
   );
