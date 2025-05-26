b09c3d6bf91ddf6f56e27ccfac3ab27f64a8db87
Kalenga243 <kevinkalenga10@gmail.com>
2025-05-26 17:07:43 +0200

    Refactor Create Profile

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,27 +1,45 @@
+"use client"
+import { SubmitButton } from "@/components/form/Button"
+import FormContainer from "@/components/form/FormContainer"
+import FormInput from "@/components/form/FormInput"
+import { useFormState } from "react-dom"
 
 
 
 
 
-const CreateProfileAction = async (formData: FormData) => {
-   'use server'
+
+const CreateProfileAction = async (prevState:any, formData: FormData) => {
+//    'use server'
+
+   // ⬇️ ici on initialise bien useFormState AVANT le JSX
+ 
    
    const firstName = formData.get('firstName') as string
-   console.log(firstName)
+   if(firstName !== 'Jean') return {message: 'There was an error'};
+   console.log(firstName) 
+   return {message: 'Profile Created'}
 }
 
 
 function CreateProfilePage() {
-  return (
+  
+  
+  
+    return (
     <section>
         <h1 className='text-2xl font-semibold mb-8 capitalize'>New user</h1>
-        <div className='border p-8 rounded-md max-w-lg'>
-            <form action={CreateProfileAction}>
-                
+        <div className='border p-8 rounded-md'>
+              <FormContainer action={CreateProfileAction}>
+                <div className="grid gap-4 mt-4">
+                    <FormInput type="text" name='firstName' label='First Name'/>
+                    <FormInput type="text" name='lastName' label='Last Name'/>
                     
-                 
-                 
-            </form>
+                    <FormInput type="text" name='username' label='Username'/>
+                </div>
+                  <SubmitButton text="Create Profile" className="mt-8" />  
+                  
+              </FormContainer>
         </div>
     </section>
   )
