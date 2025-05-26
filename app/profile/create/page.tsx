035041c83edec8a7d5472797c65011d5b1d7c7b0
Kalenga243 <kevinkalenga10@gmail.com>
2025-05-26 13:31:17 +0200

    Create profile - Setup

---
+++ app/profile/create/page.tsx
@@ -0,0 +1,33 @@
+
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+const CreateProfileAction = async (formData: FormData) => {
+   'use server'
+   
+   const firstName = formData.get('firstName') as string
+   console.log(firstName)
+}
+
+
+function CreateProfilePage() {
+  return (
+    <section>
+        <h1 className='text-2xl font-semibold mb-8 capitalize'>New user</h1>
+        <div className='border p-8 rounded-md max-w-lg'>
+            <form action={CreateProfileAction}>
+                 <div className='mb-2'>
+                     <Label htmlFor="firstName">First Name</Label>
+                     <Input id='firstName' name='firstName' type='text' />
+                 </div>
+                 <Button type='submit' size='lg'>
+                     Create Profile
+                 </Button>
+            </form>
+        </div>
+    </section>
+  )
+}
+
+export default CreateProfilePage
